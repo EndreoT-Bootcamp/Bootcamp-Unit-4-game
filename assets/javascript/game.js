213489a029747e45d8cc1bbad7e50a6482a82cac
EndreoT <endreotm@gmail.com>
2019-03-18 11:06:25 -0700

    adding comments. Cleaning up code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,33 +1,36 @@
-const charactersInitialState = 
-    {
-        "Obi-Wan-Kenobi": {
-            name: "Obi-Wan-Kenobi",
-            hp: 110,
-            "attack-power": 6,
-            "counter-attack-power": 6
-        },
-        "Luke-Skywalker": {
-            name: "Luke-Skywalker",
-            hp: 100,
-            "attack-power": 8,
-            "counter-attack-power": 8
-        },
-        "Darth-Sidious": {
-            name: "Darth-Sidious",
-            hp: 115,
-            "attack-power": 10,
-            "counter-attack-power": 10
-        },
-        "Darth-Maul": {
-            name: "Darth-Maul",
-            hp: 120,
-            "attack-power": 12,
-            "counter-attack-power": 12
-        }
+// Initial state of all characters that will not change
+const charactersInitialState =
+{
+    "Obi-Wan-Kenobi": {
+        name: "Obi-Wan-Kenobi",
+        hp: 110,
+        "attack-power": 6,
+        "counter-attack-power": 6
+    },
+    "Luke-Skywalker": {
+        name: "Luke-Skywalker",
+        hp: 100,
+        "attack-power": 8,
+        "counter-attack-power": 8
+    },
+    "Darth-Sidious": {
+        name: "Darth-Sidious",
+        hp: 115,
+        "attack-power": 10,
+        "counter-attack-power": 10
+    },
+    "Darth-Maul": {
+        name: "Darth-Maul",
+        hp: 120,
+        "attack-power": 12,
+        "counter-attack-power": 12
+    }
 };
 
+// Deep copy of character stats to be modified in fight actions
 let charactersDeepCopy = {};
 
+// State of the game
 const game = {
     state: {
         finished: false,
@@ -40,10 +43,10 @@ const game = {
     totalCharacters: 0,
 
     victory() {
-        return this.defeatedCharacters === this.totalCharacters - 1
+        return this.defeatedCharacters === this.totalCharacters - 1;
     },
 
-    fight(selectedCharacter, defender) { // (str, str)
+    fight(selectedCharacter, defender) { // (str, str) -> Null
         const chosenCharacterAttack = charactersDeepCopy[selectedCharacter]["attack-power"];
         charactersDeepCopy[selectedCharacter].hp -= charactersDeepCopy[defender]["counter-attack-power"];
         charactersDeepCopy[defender].hp -= chosenCharacterAttack;
@@ -82,7 +85,6 @@ $(document).ready(function () {
                 $("#defender").append(box);
                 game.selectedDefender = box.attr("name");
             }
-
         })
     }
 
@@ -97,7 +99,7 @@ $(document).ready(function () {
         });
 
         // Initialize all-character section with character boxes
-        charactersDeepCopy = JSON.parse(JSON.stringify(charactersInitialState));;
+        charactersDeepCopy = JSON.parse(JSON.stringify(charactersInitialState));
         Object.values(charactersDeepCopy).forEach(stats => {
             game.totalCharacters++;
             const characterBox = $("<div>");
@@ -109,7 +111,7 @@ $(document).ready(function () {
             hp.text("Health: " + stats["hp"]);
             hp.attr("class", "hp");
             attack.text("Attack: " + stats["attack-power"]);
-            attack.attr("class", "attack")
+            attack.attr("class", "attack");
 
             characterBox
                 .addClass("character-box")
@@ -117,7 +119,7 @@ $(document).ready(function () {
                 .attr("id", stats.name)
                 .append(name)
                 .append(hp)
-                .append(attack)
+                .append(attack);
 
             bindCharacterClick(characterBox);
 
@@ -125,26 +127,27 @@ $(document).ready(function () {
         });
     }
 
-    resetGame();
 
+    // Resets game display and internal game state
     $("#reset-game").click(() => {
         resetGame();
     });
 
+    // Logic and display handling fight button click
     $("#fight-button").click(() => {
         if (game.state.finished) {
-            return
+            return;
         }
         if (!game.selectedCharacter || !game.selectedDefender) {
             $("#fight-result").text("No enemy here.")
-            return
+            return;
         }
-        const selectedCharacterStr = game.selectedCharacter
-        const defenderStr = game.selectedDefender
-        const selectedCharacter = charactersDeepCopy[selectedCharacterStr]
-        const defender = charactersDeepCopy[defenderStr]
+        const selectedCharacterStr = game.selectedCharacter;
+        const defenderStr = game.selectedDefender;
+        const selectedCharacter = charactersDeepCopy[selectedCharacterStr];
+        const defender = charactersDeepCopy[defenderStr];
         const yourAttack = selectedCharacter["attack-power"];
-        const defenderAttack = defender["counter-attack-power"]
+        const defenderAttack = defender["counter-attack-power"];
 
         game.fight(selectedCharacterStr, defenderStr);
 
@@ -156,32 +159,38 @@ $(document).ready(function () {
             $("#defender").empty();
             game.selectedDefender = "";
             game.defeatedCharacters++;
-            $("#fight-result").text("You have defeated " + defenderStr + "! Choose another enemy to continue.")
+            $("#fight-result").text("You have defeated " + defenderStr + "! Choose another enemy to continue.");
 
         } else {
             $("#" + defenderStr)
                 .children(".hp")
                 .text("Health: " + defender.hp);
             $("#fight-result").text("You attacked " + defenderStr + " for " + yourAttack + " damage. "
-                + defenderStr + " attacked you for back for " + defenderAttack + " damage.")
+                + defenderStr + " attacked you for back for " + defenderAttack + " damage.");
         }
 
         const selectedCharacterObj = $("#" + selectedCharacterStr);
         selectedCharacterObj
             .children(".hp")
-            .text("Health: " + selectedCharacter.hp)
+            .text("Health: " + selectedCharacter.hp);
 
         selectedCharacterObj
             .children(".attack")
             .text("Attack: " + selectedCharacter["attack-power"]);
 
         if (game.state.loss) {
-            $("#fight-result").text(defenderStr + " defeated you. GAME OVER! Press restart to try again.")
+            $("#fight-result").text(defenderStr + " defeated you. GAME OVER! Press restart to try again.");
         }
         else if (game.victory()) {
-            game.state.finished = true;
-            game.state.win = true
-            $("#fight-result").text("You won! GAME OVER! Press restart to play again.")
+            game.setGameState(true, true, false)
+            $("#fight-result").text("You won! GAME OVER! Press restart to play again.");
         }
     });
+
+    function main() {
+        resetGame();
+    }
+
+    // Begins game
+    main();
 });
